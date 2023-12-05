0b6a7a64c042610a7938a062ffb718be7f3c0450
Ty Pacchione <pacchione.t@northeastern.edu>
2023-12-05 00:16:35 -0500

    Assignment 6

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 import "dotenv/config";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
